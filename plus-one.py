0245df95c9d46b5f6afd11ebbd19ccd27ce73ee7
Raunak Khanna <raunaksachinkhanna@gmail.com>
2025-07-28 11:12:13 -0700

    New files

--- plus-one.py
+++ plus-one.py
@@ -1,10 +1,9 @@
 class Solution(object):
     def plusOne(self, digits):
-        for i in range(len(digits)-1, -1, -1):
-            if digits[i] != 9:
-                digits[i] += 1
-                return digits
-            digits[i] = 0
-        return [1] + digits
+            if digits[-1]==9:
+                  return [1,0]
+            digits[-1]+=1
+            return digits
+        
 
         
